ca47cc18d3a390b0874d71ee4cd744f4d38f313f
Ronald Rechenmacher <ron@fnal.gov>
2026-03-10 19:21:07 +0000

    add ron003/script.git

--- .devcontainer/bootstrap-repos.sh
+++ .devcontainer/bootstrap-repos.sh
@@ -36,6 +36,7 @@ mkdir -p "${BUILD_ROOT}" "${INSTALL_ROOT}"
 REPOS=(
   "https://github.com/DUNE-DAQ/detdataformats.git|coredaq-v5.4.3|detdataformats"
   "https://github.com/DUNE-DAQ/fddetdataformats.git|fddaq-v5.4.3|fddetdataformats"
+  "https://github.com/ron003/scripts.git|main|scripts"
 )
 
 if [[ ${#REPOS[@]} -eq 0 ]]; then
